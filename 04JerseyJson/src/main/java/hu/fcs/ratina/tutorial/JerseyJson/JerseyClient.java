b3f41557ee0caa6b9acc86a55e6da382c8594497
Csaba Fülöp <csaba.fulop@nokia.com>
2015-11-12 08:45:50 +0100

    adding 04JerseyJson

---
+++ 04JerseyJson/src/main/java/hu/fcs/ratina/tutorial/JerseyJson/JerseyClient.java
@@ -0,0 +1,51 @@
+
+package hu.fcs.ratina.tutorial.JerseyJson;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+
+public class JerseyClient {
+
+    public static void main(String[] args) {
+        try {
+
+            Student st = new Student("Viola", "Fulop", 2, 1);
+
+            ClientConfig clientConfig = new DefaultClientConfig();
+
+            clientConfig.getFeatures().put(
+                    JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+
+            Client client = Client.create(clientConfig);
+
+            WebResource webResource = client
+                    .resource("http://localhost:8080/04JerseyJson/rest/jsonServices/send");
+
+            ClientResponse response = webResource
+                    .accept("application/json")
+                    .type("application/json")
+                    .post(ClientResponse.class, st);
+            // .post(ClientResponse.class, "{\"id\":2,\"firstName\":\"Zita\",\"lastName\":\"Fulop\",\"age\":1}");
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+            }
+
+            String output = response.getEntity(String.class);
+
+            System.out.println("Server response .... \n");
+            System.out.println(output);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
