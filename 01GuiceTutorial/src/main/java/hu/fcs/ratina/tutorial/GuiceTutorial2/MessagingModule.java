7f46f18e06c1970c794dd0ccdf59b6dfece2159d
Csaba Fülöp <csaba.fulop@nokia.com>
2015-11-16 13:22:45 +0100

    using @Provides method to create FacebookMessageService

--- 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MessagingModule.java
+++ 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MessagingModule.java
@@ -1,16 +1,30 @@
 
 package hu.fcs.ratina.tutorial.GuiceTutorial2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.name.Names;
 
 public class MessagingModule extends AbstractModule {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MessagingModule.class);
 
     @Override
     protected void configure() {
         bind(MessageService.class).to(EmailMessageService.class);
         bind(MessageService.class).annotatedWith(Facebook.class).to(FacebookMessageService.class);
         bind(MessageService.class).annotatedWith(Names.named("Facebook")).to(FacebookMessageService.class);
+    }
+
+    @Provides
+    FacebookMessageService provideFacebookService() {
+        LOGGER.info("Creating FacebookMessageService...");
+        FacebookMessageService service = new FacebookMessageService();
+        // set optional parameters, credetials, server, etc...
+        return service;
     }
 
 }
