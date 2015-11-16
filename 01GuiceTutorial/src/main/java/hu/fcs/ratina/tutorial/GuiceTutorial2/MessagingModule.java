d6cd0f81bee5b3e063551269ca879a0426484b2f
Csaba Fülöp <csaba.fulop@nokia.com>
2015-11-16 13:11:23 +0100

    binding annotation

--- 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MessagingModule.java
+++ 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MessagingModule.java
@@ -8,6 +8,7 @@ public class MessagingModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(MessageService.class).to(EmailMessageService.class);
+        bind(MessageService.class).annotatedWith(Facebook.class).to(FacebookMessageService.class);
     }
 
 }
--- 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MyApp.java
+++ 01GuiceTutorial/src/main/java/hu/fcs/ratina/tutorial/GuiceTutorial2/MyApp.java
@@ -8,7 +8,7 @@ public class MyApp {
     private MessageService messageService;
 
     @Inject
-    public MyApp(MessageService messageService) {
+    public MyApp(@Facebook MessageService messageService) {
         this.messageService = messageService;
     }
 
